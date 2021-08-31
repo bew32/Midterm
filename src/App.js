3d61794312622f6f5caa4db02427ca9f0118bcc4
bew32 <tawatninja425@gmail.com>
2021-08-31 18:44:01 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Navbar from './page/Navbar'
+import Header from './page/Header'
+import Homepage from './page/Homepage'
+import Footer from './page/Footer'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Header/>
+      <Homepage/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
